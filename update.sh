034a3c1f36fe289ef603788050500674d3111e84
Elvea Huang <elvea.huang@gmail.com>
2022-03-19 12:12:43 +0800

    一直在整理，一直在重构，不是在整理就是在重构。

--- update.sh
+++ update.sh
@@ -2,15 +2,20 @@
 
 # shellcheck disable=SC2164
 ROOT=$(cd "$(dirname "$0")";pwd)
-
+# ======================================================================================================================
 # commons
+# ======================================================================================================================
+# @platform/commons
 cd "$ROOT/packages/commons"
 pwd
 rm -rf package-lock.json
 rm -rf pnpm-lock.yaml
 rm -rf node_modules
 ncu -u
-# api
+# ======================================================================================================================
+# server
+# ======================================================================================================================
+# @platform/server
 cd "$ROOT/packages/server"
 pwd
 rm -rf package-lock.json
@@ -19,21 +24,53 @@ rm -rf node_modules
 rm -rf dist
 rm -rf build
 ncu -u
-# mobile
-cd "$ROOT/packages/www/commons"
+# ======================================================================================================================
+# www-commons
+# ======================================================================================================================
+# @platform/www-commons
+cd "$ROOT/packages/www-commons"
 pwd
 rm -rf package-lock.json
 rm -rf pnpm-lock.yaml
 rm -rf node_modules
 ncu -u
-# webapp
-cd "$ROOT/packages/www/webapp"
+# ======================================================================================================================
+# react
+# ======================================================================================================================
+# @platform-react/commons
+cd "$ROOT/packages/www-react/commons"
 pwd
 rm -rf package-lock.json
 rm -rf pnpm-lock.yaml
 rm -rf node_modules
 ncu -u
+# @platform-react/commons
+cd "$ROOT/packages/www-react/webapp"
+pwd
+rm -rf package-lock.json
+rm -rf pnpm-lock.yaml
+rm -rf node_modules
+ncu -u
+# ======================================================================================================================
+# vue
+# ======================================================================================================================
+# @platform-vue/commons
+cd "$ROOT/packages/www-vue/commons"
+pwd
+rm -rf package-lock.json
+rm -rf pnpm-lock.yaml
+rm -rf node_modules
+ncu -u
+# @platform-vue/commons
+cd "$ROOT/packages/www-vue/webapp"
+pwd
+rm -rf package-lock.json
+rm -rf pnpm-lock.yaml
+rm -rf node_modules
+ncu -u
+# ======================================================================================================================
 # root
+# ======================================================================================================================
 cd "$ROOT"
 pwd
 rm -rf package-lock.json
